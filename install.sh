873fdac7b4851d0e83c7902d6b2cf5986ca40203
emocat <839194377@qq.com>
2024-08-05 12:50:52 +0800

    upload environment

--- install.sh
+++ install.sh
@@ -15,16 +15,8 @@ export PATH="~/miniconda3/bin:${PATH}"
 conda init && conda create -n viohawk python=3.7 -y -q
 source ~/miniconda3/bin/activate viohawk
 
-# lgsvl
-cd PythonAPI
-pip install -r requirements.txt --user .
-cd ..
-
-# viohawk
-cd VioHawk
 pip install -r requirements.txt
 pip install opencv-python-headless
-cd ..
 
 patch ~/miniconda3/envs/viohawk/lib/python3.7/site-packages/commonroad_reach/utility/coordinate_system.py coordinate_system.py.patch
 patch ~/miniconda3/envs/viohawk/lib/python3.7/site-packages/commonroad_reach/data_structure/configuration.py configuration.py.patch
